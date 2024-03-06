3df6dcb7fea6a9113c444f00a59c64e35f067b26
Cody Taylor <codemister99@yahoo.com>
2024-03-06 11:48:55 -0500

    feat: Move tunnel spec into the Command type

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,7 +1,9 @@
 import * as fs from 'node:fs/promises';
 import express, {type Request, type Response} from 'express';
 import {config} from './config.js';
-import {type TunnelDestination, type TunnelSpecifier, sendCommand} from './tunnel-command.js';
+import {type TunnelDestination, type TunnelSpecifier, type TunnelCommand, sendCommand} from './tunnel-command.js';
+
+type TunnelRoute = `/tunnels/:${keyof TunnelDestination}`;
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -28,24 +30,27 @@ router.get('/tunnels', (_req, res, next) => {
 	listOpenTunnels().then(tunnels => res.status(200).json({tunnels}), next);
 });
 
-router.put('/tunnels/:remoteName', (req: Request<TunnelDestination, string, TunnelSpecifier>, res, next) => {
-	const command = 'start';
+const tunnelRoute = router.route<TunnelRoute>('/tunnels/:remoteHost');
 
-	req.log.info({command}, 'requesting to %s tunnel', command);
-	sendCommand(appConfig, command, req.params, req.body).then(textOk(res), next);
-});
+tunnelRoute.put((req: Request<TunnelDestination, string, TunnelSpecifier>, res, next) => {
+	// TODO validate req.body, there's no runtime type safety.
+	const command: TunnelCommand = {cmd: 'start', tunnel: req.body};
 
-router.get('/tunnels/:remoteName', (req: Request<TunnelDestination, string>, res, next) => {
-	const command = 'status';
-
-	req.log.info({command}, 'requesting to %s tunnel', command);
+	req.log.info({command}, 'requesting to %s tunnel', command.cmd);
 	sendCommand(appConfig, command, req.params).then(textOk(res), next);
 });
 
-router.delete('/tunnels/:remoteName', (req: Request<TunnelDestination, string>, res, next) => {
-	const command = 'stop';
+tunnelRoute.get((req: Request<TunnelDestination, string>, res, next) => {
+	const command: TunnelCommand = {cmd: 'status'};
 
-	req.log.info({command}, 'requesting to %s tunnel', command);
+	req.log.info({command}, 'requesting to %s tunnel', command.cmd);
+	sendCommand(appConfig, command, req.params).then(textOk(res), next);
+});
+
+tunnelRoute.delete((req: Request<TunnelDestination, string>, res, next) => {
+	const command: TunnelCommand = {cmd: 'stop'};
+
+	req.log.info({command}, 'requesting to %s tunnel', command.cmd);
 	sendCommand(appConfig, command, req.params).then(textOk(res), next);
 });
 
--- src/app/tunnel-command.ts
+++ src/app/tunnel-command.ts
@@ -2,11 +2,6 @@ import * as net from 'node:net';
 import {text} from 'node:stream/consumers';
 import type {AppConfiguration} from './config.js';
 
-type TunnelCommand =
-	| 'start'
-	| 'status'
-	| 'stop';
-
 export type TunnelDestination = {
 	remoteHost: string;
 };
@@ -17,15 +12,19 @@ export type TunnelSpecifier = {
 	tunnelPort: number;
 };
 
-export async function sendCommand(config: AppConfiguration, command: TunnelCommand, {remoteHost}: TunnelDestination, specifier?: TunnelSpecifier): Promise<string> {
+export type TunnelCommand =
+	| {cmd: 'start'; tunnel: TunnelSpecifier}
+	| {cmd: 'status'}
+	| {cmd: 'stop'};
+
+export async function sendCommand(config: AppConfiguration, command: TunnelCommand, {remoteHost}: TunnelDestination): Promise<string> {
 	const socket = net.connect(config.paths.server, () => {
-		socket.write(command + '\n');
+		socket.write(command.cmd + '\n');
 		socket.write(remoteHost + '\n');
 
-		if (command === 'start' && specifier) {
-			socket.write(`${specifier.localPort}:${specifier.tunnelHost}:${specifier.tunnelPort}\n`);
-		} else {
-			throw new Error('Command "start" requires a tunnel to be specified');
+		if (command.cmd === 'start') {
+			const {tunnel} = command;
+			socket.write(`${tunnel.localPort}:${tunnel.tunnelHost}:${tunnel.tunnelPort}\n`);
 		}
 
 		socket.end();
